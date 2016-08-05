2d7d3647a36a05d91a379f4745136f666f1b1c6e
Shyam Sunder <ssunde@codeaurora.org>
2016-08-05 20:55:12 +0530

    [qca-nss-ecm] PPTP GRE pass-through mode fix
    if it's a PPTP GRE pass-through flow, don't accelerate it
    
    Change-Id: I17e5e2c9fc97ee1364b7774a96c92263cf6740b8
    Signed-off-by: Shyam Sunder <ssunde@codeaurora.org>

--- ecm_interface.c
+++ ecm_interface.c
@@ -1020,15 +1020,14 @@ struct neighbour *ecm_interface_ipv6_neigh_get(ip_addr_t addr)
 }
 #endif
 
-#ifdef ECM_INTERFACE_PPP_ENABLE
 /*
- * ecm_interface_skip_pptp()
+ * ecm_interface_is_pptp()
  *	skip pptp tunnel encapsulated traffic
  *
  * ECM does not handle PPTP,
  * this function detects packets of that type so they can be skipped over to improve their throughput.
  */
-bool ecm_interface_skip_pptp(struct sk_buff *skb, const struct net_device *out)
+bool ecm_interface_is_pptp(struct sk_buff *skb, const struct net_device *out)
 {
 	struct ppp_channel *ppp_chan[1];
 	int px_proto;
@@ -1081,14 +1080,15 @@ bool ecm_interface_skip_pptp(struct sk_buff *skb, const struct net_device *out)
 	return false;
 }
 
+#ifdef ECM_INTERFACE_PPP_ENABLE
 /*
- * ecm_interface_skip_l2tp_packet_by_version()
+ * ecm_interface_is_l2tp_packet_by_version()
  *	Check version of l2tp tunnel encapsulated traffic
  *
  * ECM does not handle l2tp,
  * this function detects packets of that type so they can be skipped over to improve their throughput.
  */
-bool ecm_interface_skip_l2tp_packet_by_version(struct sk_buff *skb, const struct net_device *out, int ver)
+bool ecm_interface_is_l2tp_packet_by_version(struct sk_buff *skb, const struct net_device *out, int ver)
 {
 	struct ppp_channel *ppp_chan[1];
 	int px_proto;
@@ -1157,13 +1157,13 @@ bool ecm_interface_skip_l2tp_packet_by_version(struct sk_buff *skb, const struct
 }
 
 /*
- * ecm_interface_skip_l2tp_pptp()
+ * ecm_interface_is_l2tp_pptp()
  *	skip l2tp/pptp tunnel encapsulated traffic
  *
  * ECM does not handle L2TP or PPTP encapsulated packets,
  * this function detects packets of that type so they can be skipped over to improve their throughput.
  */
-bool ecm_interface_skip_l2tp_pptp(struct sk_buff *skb, const struct net_device *out)
+bool ecm_interface_is_l2tp_pptp(struct sk_buff *skb, const struct net_device *out)
 {
 	struct ppp_channel *ppp_chan[1];
 	int px_proto;
