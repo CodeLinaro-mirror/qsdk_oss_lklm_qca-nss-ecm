fabdc74bcf5d302829325016fdb6f8d5a98bacc9
Murat Sezgin <msezgin@codeaurora.org>
2017-07-07 14:53:32 -0700

    [qca-nss-ecm] Do not handle fdb change events for SFE.
    SFE is a L3 accelaration engine and no need to handle
    the L2 notifications.
    
    Change-Id: I593e7f7571669b9114c869ddd7472c0cb67a1aac
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- ecm_interface.c
+++ ecm_interface.c
@@ -5343,9 +5343,10 @@ static int ecm_interface_node_br_fdb_notify_event(struct notifier_block *nb,
 {
 	uint8_t *mac =  (uint8_t *)data;
 
-	DEBUG_INFO("FDB updated for node %pM\n", mac);
-
-	ecm_interface_node_connections_defunct(mac);
+	if (ECM_FRONT_END_TYPE_NSS == ecm_front_end_type_get()) {
+		DEBUG_INFO("FDB updated for node %pM\n", mac);
+		ecm_interface_node_connections_defunct(mac);
+	}
 
 	return NOTIFY_DONE;
 }
