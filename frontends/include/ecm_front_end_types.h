a2b5fcaa522dc9abefb1f25bf3f69de458e794dd
Suman Ghosh <sumaghos@codeaurora.org>
2018-09-17 15:15:38 +0530

    [qca-nss-ecm] Modify ipq60xx to ipq6018
    Change-Id: Ibbadddcccd1c50691b3b8f72c2f9545043589b84
    Signed-off-by: Suman Ghosh <sumaghos@codeaurora.org>

--- frontends/include/ecm_front_end_types.h
+++ frontends/include/ecm_front_end_types.h
@@ -224,7 +224,7 @@ static inline enum ecm_front_end_type ecm_front_end_type_get(void)
 	bool nss_supported = of_machine_is_compatible("qcom,ipq8064") ||
 				of_machine_is_compatible("qcom,ipq8062") ||
 				of_machine_is_compatible("qcom,ipq807x") ||
-				of_machine_is_compatible("qcom,ipq60xx");
+				of_machine_is_compatible("qcom,ipq6018");
 #else
 	bool nss_supported = true;
 #endif
