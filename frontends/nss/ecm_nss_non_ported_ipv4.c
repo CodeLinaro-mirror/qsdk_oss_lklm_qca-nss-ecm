f610e4f31d33c1df9b5aa7cb33fb67cf827c9284
Murat Sezgin <msezgin@codeaurora.org>
2019-04-15 17:26:02 -0700

    [qca-nss-ecm] Use the correct destroy mechanism in decel pending state
    There is a situation to call the destroy function for a connection
    before that connection is fully accelerated. While it is in accel
    pending state which means waiting for the response from the acceleration
    engine. In this situation, the destroy function just returns and waits
    for the accel response. When the response comes, it should use the
    same destroy mechanism it tried before. Otherwise the state of the
    connection becomes invalid.
    
    Change-Id: I69cc8c4389bebb0331ad3b5cf1df1072b09a31d3
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- frontends/nss/ecm_nss_non_ported_ipv4.c
+++ frontends/nss/ecm_nss_non_ported_ipv4.c
@@ -210,6 +210,7 @@ static void ecm_nss_non_ported_ipv4_connection_callback(void *app_data, struct n
 	struct ecm_front_end_connection_instance *feci;
 	struct ecm_nss_non_ported_ipv4_connection_instance *nnpci;
 	ecm_front_end_acceleration_mode_t result_mode;
+	bool is_defunct = false;
 
 	/*
 	 * Is this a response to a create message?
@@ -372,10 +373,28 @@ static void ecm_nss_non_ported_ipv4_connection_callback(void *app_data, struct n
 
 	DEBUG_INFO("%p: Decelerate was pending\n", ci);
 
+	/*
+	 * Check if the pending decelerate was done with the defunct process.
+	 * If it was, set the is_defunct flag of the feci to false for re-try.
+	 */
+	if (feci->is_defunct) {
+		is_defunct = feci->is_defunct;
+		feci->is_defunct = false;
+	}
+
 	spin_unlock_bh(&ecm_nss_ipv4_lock);
 	spin_unlock_bh(&feci->lock);
 
-	feci->decelerate(feci);
+	/*
+	 * If the pending decelerate was done through defunct process, we should
+	 * re-try it here with the same defunct function, because the purpose of that
+	 * process is to remove the connection from the database as well after decelerating it.
+	 */
+	if (is_defunct) {
+		ecm_db_connection_make_defunct(ci);
+	} else {
+		feci->decelerate(feci);
+	}
 
 	/*
 	 * Release the connection.
