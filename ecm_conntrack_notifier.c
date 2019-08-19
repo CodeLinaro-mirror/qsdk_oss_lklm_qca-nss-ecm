dfe37baa1366c8dd928d62c243aa12ea3bee9749
Apoorv Gupta <apoogupt@codeaurora.org>
2019-08-19 20:39:49 +0530

    [qca-nss-ecm] Do not accelerate flows untracked by nfct.
    When the flow is set to NOTRACK target (iptables rule),
    the skb is set to dummy conntrack. This dummy ct
    (nf_conntrack_untracked) is defined as a per CPU
    variable and should be read using nf_ct_untracked_get()
    API to check if the flow is untracked. Untracked flows
    could be dropped after ECM processing, return if the flow
    is untracked.
    
    Change-Id: Iedbde902c5999441b8b4f008523ec81b63a65afb
    Signed-off-by: Apoorv Gupta <apoogupt@codeaurora.org>

--- ecm_conntrack_notifier.c
+++ ecm_conntrack_notifier.c
@@ -1,6 +1,6 @@
 /*
  **************************************************************************
- * Copyright (c) 2016-2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2017, 2019 The Linux Foundation. All rights reserved.
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
@@ -348,7 +348,7 @@ static int ecm_conntrack_event(unsigned int events, struct nf_ct_event *item)
 	/*
 	 * Special untracked connection is not monitored
 	 */
-	if (ct == &nf_conntrack_untracked) {
+	if (ct == nf_ct_untracked_get()) {
 		DEBUG_TRACE("Fake connection event - ignoring\n");
 		return NOTIFY_DONE;
 	}
