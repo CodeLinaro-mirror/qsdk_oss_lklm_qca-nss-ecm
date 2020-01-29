9ab9e66a6c12572843d80ae61cba77e59c7b99bf
Mainak Sen <smainak@codeaurora.org>
2020-01-29 12:25:18 +0530

    [qca-nss-ecm] Add flag for RMNET in ecm
    Add a flag to enable RMNET only for 1G/512mb
    profile
    
    Change-Id: I7268ebab0bb86578db8decaa21ec41345f48e3cd
    Signed-off-by: Mainak Sen <smainak@codeaurora.org>

--- frontends/nss/ecm_nss_common.h
+++ frontends/nss/ecm_nss_common.h
@@ -73,9 +73,11 @@ static inline int32_t ecm_nss_common_get_interface_number_by_dev(struct net_devi
 		return NSS_IPSEC_CMN_INTERFACE;
 	}
 
+#ifdef ECM_INTERFACE_RAWIP_ENABLE
 	if (dev->type == ARPHRD_RAWIP) {
 		return nss_rmnet_rx_get_ifnum(dev);
 	}
+#endif
 
 	return nss_cmn_get_interface_number_by_dev(dev);
 }
