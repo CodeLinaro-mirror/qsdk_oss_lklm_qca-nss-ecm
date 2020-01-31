a33d7dff4a4400271a890699d0498f84ff0f3c51
Suman Ghosh <sumaghos@codeaurora.org>
2020-01-31 21:45:20 +0530

    [qca-nss-ecm] Fix ecm_dump print issue for ovs unicast
    Change-Id: I627c7e972e4d16f33a4d066fd356326db0cc8064
    Signed-off-by: Suman Ghosh <sumaghos@codeaurora.org>

--- ecm_classifier.h
+++ ecm_classifier.h
@@ -288,13 +288,13 @@ static inline int ecm_classifier_process_response_state_get(struct ecm_state_fil
 		 * TODO: Clean up the function later to print classifier
 		 * specific data in each classifier’s state_get function.
 		 */
-		if (pr->ingress_vlan_tag[0] != 0xFFF) {
+		if (pr->ingress_vlan_tag[0] != ECM_FRONT_END_VLAN_ID_NOT_CONFIGURED) {
 			if ((result = ecm_state_write(sfi, "ingress_vlan_tag[0]", "0x%x", pr->ingress_vlan_tag[0]))) {
 				return result;
 			}
 		}
 
-		if (pr->egress_vlan_tag[0] != 0xFFF) {
+		if (pr->egress_vlan_tag[0] != ECM_FRONT_END_VLAN_ID_NOT_CONFIGURED) {
 			if ((result = ecm_state_write(sfi, "egress_vlan_tag[0]", "0x%x", pr->egress_vlan_tag[0]))) {
 				return result;
 			}
@@ -303,7 +303,7 @@ static inline int ecm_classifier_process_response_state_get(struct ecm_state_fil
 #ifdef ECM_MULTICAST_ENABLE
 		for (i = 0; i < ECM_DB_MULTICAST_IF_MAX; i++) {
 
-			if (pr->egress_mc_vlan_tag[i][0] == 0xFFF) {
+			if (pr->egress_mc_vlan_tag[i][0] == ECM_FRONT_END_VLAN_ID_NOT_CONFIGURED) {
 				continue;
 			}
 
@@ -327,13 +327,13 @@ static inline int ecm_classifier_process_response_state_get(struct ecm_state_fil
 		 * TODO: Clean up the function later to print classifier
 		 * specific data in each classifier’s state_get function.
 		 */
-		if (pr->ingress_vlan_tag[1] != 0xFFF) {
+		if (pr->ingress_vlan_tag[1] != ECM_FRONT_END_VLAN_ID_NOT_CONFIGURED) {
 			if ((result = ecm_state_write(sfi, "ingress_vlan_tag[1]", "0x%x", pr->ingress_vlan_tag[1]))) {
 				return result;
 			}
 		}
 
-		if (pr->egress_vlan_tag[1] != 0xFFF) {
+		if (pr->egress_vlan_tag[1] != ECM_FRONT_END_VLAN_ID_NOT_CONFIGURED) {
 			if ((result = ecm_state_write(sfi, "egress_vlan_tag[1]", "0x%x", pr->egress_vlan_tag[1]))) {
 				return result;
 			}
@@ -342,7 +342,7 @@ static inline int ecm_classifier_process_response_state_get(struct ecm_state_fil
 #ifdef ECM_MULTICAST_ENABLE
 		for (i = 0; i < ECM_DB_MULTICAST_IF_MAX; i++) {
 
-			if (pr->egress_mc_vlan_tag[i][1] == 0xFFF) {
+			if (pr->egress_mc_vlan_tag[i][1] == ECM_FRONT_END_VLAN_ID_NOT_CONFIGURED) {
 				continue;
 			}
 
