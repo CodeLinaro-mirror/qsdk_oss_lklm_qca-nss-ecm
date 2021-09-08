f9f771ade8979174df55e22633849f938116d2b1
Sneha Maganahalli <smaganah@codeaurora.org>
2021-09-08 11:05:23 +0530

    [qca-nss-ecm] Avoid Dslite outer rule from overwriting QoS classification.
    Dslite's outer rule was setting QOS_VALID flag that caused the rule to
    overwrite the classification done on the inner flows. This change
    avoids that from happening by not setting QOS_VALID flag on the outer
    rule.
    
    Change-Id: I2788a175361ab55b4f423292d51cdf0017491620
    signed-off-by: Sneha Maganahalli <smaganah@codeaurora.org>

--- frontends/nss/ecm_nss_non_ported_ipv6.c
+++ frontends/nss/ecm_nss_non_ported_ipv6.c
@@ -748,6 +748,17 @@ static void ecm_nss_non_ported_ipv6_connection_accelerate(struct ecm_front_end_c
 			DEBUG_TRACE("%px: IPSEC - unsupported\n", nnpci);
 #endif
 			break;
+		case ECM_DB_IFACE_TYPE_TUNIPIP6:
+#ifdef ECM_INTERFACE_TUNIPIP6_ENABLE
+			dev = dev_get_by_index(&init_net, ecm_db_iface_interface_identifier_get(ii));
+			if (dev) {
+				if (ecm_nss_common_get_interface_type(feci, dev) == NSS_DYNAMIC_INTERFACE_TYPE_TUNIPIP6_OUTER) {
+						nircm->valid_flags &= ~NSS_IPV6_RULE_CREATE_QOS_VALID;
+				}
+				dev_put(dev);
+			}
+#endif
+			break;
 		default:
 			DEBUG_TRACE("%px: Ignoring: %d (%s)\n", nnpci, ii_type, ii_name);
 		}
