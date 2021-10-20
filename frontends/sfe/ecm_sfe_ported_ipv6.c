b696e09da310c6c2044762c1529ab430962d06ae
Ratheesh Kannoth <quic_rkannoth@quicinc.com>
2021-10-20 14:02:18 +0530

    [qca-nss-ecm] support for src mac message in rule create
    Change-Id: I9fd1775ff6354ea1aecb9054234ee2553ed3d9c8
    Signed-off-by: Ratheesh Kannoth <quic_rkannoth@quicinc.com>

--- frontends/sfe/ecm_sfe_ported_ipv6.c
+++ frontends/sfe/ecm_sfe_ported_ipv6.c
@@ -491,6 +491,11 @@ static void ecm_sfe_ported_ipv6_connection_accelerate(struct ecm_front_end_conne
 				break;
 			}
 			ecm_db_iface_bridge_address_get(ii, from_sfe_iface_address);
+			if (is_valid_ether_addr(from_sfe_iface_address)) {
+				ether_addr_copy((uint8_t *)nircm->src_mac_rule.flow_src_mac, from_sfe_iface_address);
+				nircm->src_mac_rule.mac_valid_flags |= SFE_SRC_MAC_FLOW_VALID;
+				nircm->valid_flags |= SFE_RULE_CREATE_SRC_MAC_VALID;
+			}
 			DEBUG_TRACE("%px: Bridge - mac: %pM\n", npci, from_sfe_iface_address);
 			break;
 
@@ -677,6 +682,12 @@ static void ecm_sfe_ported_ipv6_connection_accelerate(struct ecm_front_end_conne
 				break;
 			}
 			ecm_db_iface_bridge_address_get(ii, to_sfe_iface_address);
+
+			if (is_valid_ether_addr(to_sfe_iface_address)) {
+				ether_addr_copy((uint8_t *)nircm->src_mac_rule.return_src_mac, to_sfe_iface_address);
+				nircm->src_mac_rule.mac_valid_flags |= SFE_SRC_MAC_RETURN_VALID;
+				nircm->valid_flags |= SFE_RULE_CREATE_SRC_MAC_VALID;
+			}
 			DEBUG_TRACE("%px: Bridge - mac: %pM\n", npci, to_sfe_iface_address);
 			break;
 
