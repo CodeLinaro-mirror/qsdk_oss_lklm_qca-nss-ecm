a6245a08a4d294ded0398880962f13acec8da78f
Ken Zhu <quic_guigenz@quicinc.com>
2022-04-15 12:43:53 -0700

    [qca-nss-ecm] Fix the loop when fdb is refreshed.
    There is a loop when fdb is refreshed:
    br_refresh_fdb_entry --> ecm_interface_node_connections_defunct -->
    sfe_ipv4_flush_connection --> ecm_interface_stats_update -->
    br_refresh_fdb_entry.
    
    Add br_fdb_entry_refresh function, it update entry's timestamp
    only when it exists.
    
    Change-Id: I20446824143d05810f17bed756bc2989d7236e1c
    Signed-off-by: Ken Zhu <quic_guigenz@quicinc.com>

--- ecm_interface.c
+++ ecm_interface.c
@@ -6462,15 +6462,12 @@ static void ecm_interface_list_stats_update(int iface_list_first, struct ecm_db_
 			 */
 			if ((is_ported || ecm_db_connection_is_pppoe_bridged_get(ci)) &&
 				is_valid_ether_addr(mac_addr) && ecm_front_end_is_bridge_port(dev) && rx_packets) {
-				DEBUG_TRACE("Update bridge fdb entry for mac: %pM\n", mac_addr);
 
+				DEBUG_TRACE("Update bridge fdb entry for mac: %pM\n", mac_addr);
 				/*
-				 * Update fdb entry only if it exist. Please note that br_refresh_fdb_entry() API
-				 * creates new fdb entry if it does not exist.
+				 * Update the existing fdb entry's timestamp only.
 				 */
-				if (br_fdb_has_entry(dev, mac_addr, 0)) {
-					br_refresh_fdb_entry(dev, mac_addr);
-				}
+				br_fdb_entry_refresh(dev, mac_addr, 0);
 			}
 		}
 
