d5e6d61516427b87055a14ad981fe1cf723afd1e
Parikshit Gune <quic_pgune@quicinc.com>
2022-04-18 13:15:44 +0530

    [qca-nss-ecm] Fast xmit check is changed and done on destination netdev instead of source netdev.
    Change-Id: I83bb4eaed59cbb90c54bcbaafba6b5ec16a0e2fd
    Signed-off-by: Parikshit Gune <quic_pgune@quicinc.com>

--- frontends/sfe/ecm_sfe_ported_ipv4.c
+++ frontends/sfe/ecm_sfe_ported_ipv4.c
@@ -354,7 +354,7 @@ static void ecm_sfe_ipv4_fast_xmit_set(struct sfe_ipv4_rule_create_msg *msg)
 		interface_num = msg->conn_rule.flow_interface_num;
 	}
 	if (ecm_sfe_common_fast_xmit_check(interface_num)) {
-		msg->rule_flags |= SFE_RULE_CREATE_FLAG_FLOW_TRANSMIT_FAST;
+		msg->rule_flags |= SFE_RULE_CREATE_FLAG_RETURN_TRANSMIT_FAST;
 	}
 
 	interface_num = msg->conn_rule.return_top_interface_num;
@@ -362,7 +362,7 @@ static void ecm_sfe_ipv4_fast_xmit_set(struct sfe_ipv4_rule_create_msg *msg)
 		interface_num = msg->conn_rule.return_interface_num;
 	}
 	if (ecm_sfe_common_fast_xmit_check(interface_num)) {
-		msg->rule_flags |= SFE_RULE_CREATE_FLAG_RETURN_TRANSMIT_FAST;
+		msg->rule_flags |= SFE_RULE_CREATE_FLAG_FLOW_TRANSMIT_FAST;
 	}
 
 	rcu_read_unlock_bh();
