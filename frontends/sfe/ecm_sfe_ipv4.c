7998ab829f8d12247a9f3277941e7db0b5ce0140
Ken Zhu <quic_guigenz@quicinc.com>
2022-05-17 17:04:57 -0700

    [qca-nss-ecm] Don't update CT when it is destroyed
    if many CTs are expired at almost the same time, in the first CT destroy
    event handler, nf_conntrack_find_get could trigger another CT destroy event,
    this could make the call stack very long and cause unexpected behaviour.
    
    Needn't sync a destroyed ct status.
    
    Change-Id: I038920490a23c5ad3431c2989170f637abfb7458
    Signed-off-by: Ken Zhu <quic_guigenz@quicinc.com>

--- frontends/sfe/ecm_sfe_ipv4.c
+++ frontends/sfe/ecm_sfe_ipv4.c
@@ -337,8 +337,11 @@ static void ecm_sfe_ipv4_process_one_conn_sync_msg(struct sfe_ipv4_conn_sync *sy
 		 * for the sync message which comes as a final sync for the ECM initiated destroy request.
 		 * Because this means the connection is not active for sometime and adding this delta time
 		 * to the conntrack timeout will update it eventhough there is no traffic for this connection.
+		 * When the CT in destroy status, find ct could cause race
+		 * conditions
 		 */
-		if (!sync->flow_tx_packet_count && !sync->return_tx_packet_count) {
+		if ((!sync->flow_tx_packet_count && !sync->return_tx_packet_count)
+				|| (ci->flags & ECM_DB_CONNECTION_FLAGS_DEFUNCT_CT_DESTROYED)) {
 			feci->deref(feci);
 			ecm_db_connection_deref(ci);
 			return;
