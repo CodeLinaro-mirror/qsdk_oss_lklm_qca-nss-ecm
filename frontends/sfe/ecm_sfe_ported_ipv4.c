b2a800b9a17b5f619cf63d4b318bbbe28f4fda90
Amitesh Anand <quic_amitesh@quicinc.com>
2023-03-17 22:40:12 +0530

    [qca-nss-ecm] Bridge VLAN Filter code refactoring & fixes
    Changes done in this patch:
    
    1) Add debug logs during ecm create rule. And some code refactoring
    for ecm dump.
    
    2) Have common func for IPv4/IPv6 to set vlan filter rule from
    connection instance. This fixes missed out code changes in IPv6,
    which were done for IPv4.
    
    Change-Id: I580895eacbd7674a3952604c84736ff0d50beb73
    Signed-off-by: Amitesh Anand <quic_amitesh@quicinc.com>
    (cherry picked from commit 0f952af1472db930850279cc3a9076586cf7a14e)

--- frontends/sfe/ecm_sfe_ported_ipv4.c
+++ frontends/sfe/ecm_sfe_ported_ipv4.c
@@ -1,7 +1,7 @@
 /*
  **************************************************************************
  * Copyright (c) 2015-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -1195,7 +1195,16 @@ static void ecm_sfe_ported_ipv4_connection_accelerate(struct ecm_front_end_conne
 
 #ifdef ECM_BRIDGE_VLAN_FILTERING_ENABLE
 	if (feci->ci->vlan_filter_valid) {
-		ecm_sfe_common_ipv4_vlan_filter_set(feci->ci, nircm);
+		DEBUG_INFO("%px: Bridge vlan filter is valid. Updating the create rule\n", feci);
+		if (ecm_sfe_common_vlan_filter_set(feci->ci, &nircm->flow_vlan_filter_rule, true) &&
+			ecm_sfe_common_vlan_filter_set(feci->ci, &nircm->return_vlan_filter_rule, false)) {
+			/*
+			 * Both flow and return directions are valid.
+			 */
+			nircm->valid_flags |= SFE_RULE_CREATE_VLAN_FILTER_VALID;
+		} else {
+			nircm->valid_flags &= ~SFE_RULE_CREATE_VLAN_FILTER_VALID;
+		}
 	}
 
 	/*
@@ -1497,12 +1506,6 @@ static void ecm_sfe_ported_ipv4_connection_accelerate(struct ecm_front_end_conne
 			"secondary_ingress_vlan_tag: %x\n"
 			"secondary_egress_vlan_tag: %x\n"
 			"flags: rule=%x valid=%x src_mac_valid=%x\n"
-#ifdef ECM_BRIDGE_VLAN_FILTERING_ENABLE
-			"flow_vlan_filter_ingress_vlan_tag: %x, flags: %x\n"
-			"flow_vlan_filter_egress_vlan_tag: %x, flags: %x\n"
-			"return_vlan_filter_ingress_vlan_tag: %x, flags: %x\n"
-			"return_vlan_filter_egress_vlan_tag: %x, flags: %x\n"
-#endif
 			"return_pppoe_session_id: %u\n"
 			"return_pppoe_remote_mac: %pM\n"
 			"flow_pppoe_session_id: %u\n"
@@ -1545,12 +1548,6 @@ static void ecm_sfe_ported_ipv4_connection_accelerate(struct ecm_front_end_conne
 			nircm->rule_flags,
 			nircm->valid_flags,
 			nircm->src_mac_rule.mac_valid_flags,
-#ifdef ECM_BRIDGE_VLAN_FILTERING_ENABLE
-			nircm->flow_vlan_filter_rule.ingress_vlan_tag, nircm->flow_vlan_filter_rule.ingress_flags,
-			nircm->flow_vlan_filter_rule.egress_vlan_tag, nircm->flow_vlan_filter_rule.egress_flags,
-			nircm->return_vlan_filter_rule.ingress_vlan_tag, nircm->return_vlan_filter_rule.ingress_flags,
-			nircm->return_vlan_filter_rule.egress_vlan_tag, nircm->return_vlan_filter_rule.egress_flags,
-#endif
 			nircm->pppoe_rule.return_pppoe_session_id,
 			nircm->pppoe_rule.return_pppoe_remote_mac,
 			nircm->pppoe_rule.flow_pppoe_session_id,
@@ -1570,8 +1567,18 @@ static void ecm_sfe_ported_ipv4_connection_accelerate(struct ecm_front_end_conne
 			nircm->mark_rule.flow_mark,
 			nircm->mark_rule.return_mark);
 
-	if (protocol == IPPROTO_TCP) {
+#ifdef ECM_BRIDGE_VLAN_FILTERING_ENABLE
+	DEBUG_INFO("flow_vlan_filter_ingress_vlan_tag: %x, flags: %x\n"
+			"flow_vlan_filter_egress_vlan_tag: %x, flags: %x\n"
+			"return_vlan_filter_ingress_vlan_tag: %x, flags: %x\n"
+			"return_vlan_filter_egress_vlan_tag: %x, flags: %x\n",
+			nircm->flow_vlan_filter_rule.ingress_vlan_tag, nircm->flow_vlan_filter_rule.ingress_flags,
+			nircm->flow_vlan_filter_rule.egress_vlan_tag, nircm->flow_vlan_filter_rule.egress_flags,
+			nircm->return_vlan_filter_rule.ingress_vlan_tag, nircm->return_vlan_filter_rule.ingress_flags,
+			nircm->return_vlan_filter_rule.egress_vlan_tag, nircm->return_vlan_filter_rule.egress_flags);
+#endif
 
+	if (protocol == IPPROTO_TCP) {
 		DEBUG_INFO("flow_window_scale: %u\n"
 			"flow_max_window: %u\n"
 			"flow_end: %u\n"
