bf2c64c8f1806d365465f5c40b6a53fb904787c9
Murat Sezgin <quic_msezgin@quicinc.com>
2023-03-21 10:00:49 -0700

    [qca-nss-ecm] Add a Makefile for 6.1 kernel builds
    This Makefile will be used while compiling the module
    for 6.1 kernel with reduced features. Features will be added
    incrementally as we ported them to 6.1 kernel. Once we port
    all the features, this Makefile will be removed and master
    Makefile will be used for all the kernel version.
    
    Change-Id: Idaffd8db7afddba2e79eea79baa857a69c015491
    Signed-off-by: Murat Sezgin <quic_msezgin@quicinc.com>

--- Makefile_61.mk
+++ Makefile_61.mk
@@ -1,25 +1,7 @@
-##########################################################################
-# Copyright (c) 2014-2016, 2018-2021, The Linux Foundation. All rights reserved.
-# Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
-#
-# Permission to use, copy, modify, and/or distribute this software for
-# any purpose with or without fee is hereby granted, provided that the
-# above copyright notice and this permission notice appear in all copies.
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
-# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-##########################################################################
-
 # ###################################################
 # Makefile for the QCA NSS ECM
 # ###################################################
-ifneq ($(findstring 6.1., $(KERNELVERSION)),)
-include $(obj)/Makefile_61.mk
-else
+
 ifeq ($(ECM_FRONT_END_SFE_ENABLE), y)
 obj-m += examples/ecm_sfe_l2.o
 endif
@@ -109,11 +91,6 @@ ecm-$(ECM_FRONT_END_PPE_ENABLE) += frontends/ppe/ecm_ppe_common.o
 ecm-$(ECM_FRONT_END_PPE_ENABLE) += frontends/ppe/ecm_ppe_ipv4.o
 ecm-$(ECM_FRONT_END_PPE_ENABLE) += frontends/ppe/ecm_ppe_ported_ipv4.o
 ccflags-$(ECM_FRONT_END_PPE_ENABLE) += -DECM_FRONT_END_PPE_ENABLE
-
-# #############################################################################
-# Define ECM_FRONT_END_PPE_QOS_ENABLE=y in order to enable PPE QoS
-# #############################################################################
-ccflags-$(ECM_FRONT_END_PPE_QOS_ENABLE) += -DECM_FRONT_END_PPE_QOS_ENABLE
 
 # #############################################################################
 # Define ECM_FRONT_END_CONN_LIMIT_ENABLE=y in order to limit accelerated
@@ -501,4 +478,3 @@ endif
 ccflags-y += -Wall -Werror
 
 obj ?= .
-endif
