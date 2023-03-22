63d6565466acc9263297a6d8f92e3a25da66b148
Pritam Kumar Jha <quic_pritamku@quicinc.com>
2023-03-22 12:56:07 +0530

    [qca-nss-ecm] Set IPsec receive MTU to maximum for outer rule.
    Receive MTU check can't be disabled for PPE & NSS AE.
    Hence, setting MTU to maximum value.
    
    Change-Id: I3e3ff0b07e29457acbdf9bb1d49a6ad09a161104
    Signed-off-by: Pritam Kumar Jha <quic_pritamku@quicinc.com>

--- frontends/ppe/ecm_ppe_non_ported_ipv6.c
+++ frontends/ppe/ecm_ppe_non_ported_ipv6.c
@@ -478,7 +478,16 @@ static void ecm_ppe_non_ported_ipv6_connection_accelerate(struct ecm_front_end_c
 			break;
 
 		case ECM_DB_IFACE_TYPE_IPSEC_TUNNEL:
-#ifndef ECM_INTERFACE_IPSEC_ENABLE
+#ifdef ECM_INTERFACE_IPSEC_ENABLE
+			DEBUG_TRACE("%px: IPSEC\n", feci);
+
+			/*
+			 * Override the MTU size in the decap direction, this will apply to IPsec->WAN rule
+			 */
+			if (IP6CB(skb)->flags & IP6SKB_XFRM_TRANSFORMED) {
+				pd6rc->conn_rule.flow_mtu = ECM_DB_IFACE_MTU_MAX;
+			}
+#else
 			rule_invalid = true;
 			DEBUG_TRACE("%px: IPSEC - unsupported\n", feci);
 #endif
