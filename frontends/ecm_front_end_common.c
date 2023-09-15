5be788e99641a5b556db9e9274e4082c9831489a
Hardik S. Panchal <quic_hpanchal@quicinc.com>
2023-09-15 15:47:27 +0530

    [qca-nss-ecm] Deny xfrm feature when xfrm offload is not enabled.
    Change-Id: I28829e78febef90f5e433255a6a2148a05ebcd96
    Signed-off-by: Hardik S. Panchal <quic_hpanchal@quicinc.com>

--- frontends/ecm_front_end_common.c
+++ frontends/ecm_front_end_common.c
@@ -251,9 +251,9 @@ bool ecm_front_end_is_feature_supported(enum ecm_fe_feature feature)
 
 /*
  * ecm_front_end_is_xfrm_flow()
- *	Check if the flow is an xfrm flow.
+ *	Returns true if the flow is an xfrm flow and identifies if the flow is xfrm inner.
  */
-static bool ecm_front_end_is_xfrm_flow(struct sk_buff *skb, struct ecm_tracker_ip_header *ip_hdr)
+bool ecm_front_end_is_xfrm_flow(struct sk_buff *skb, struct ecm_tracker_ip_header *ip_hdr, bool *inner)
 {
 #ifdef CONFIG_XFRM
 	struct dst_entry *dst;
@@ -272,10 +272,12 @@ static bool ecm_front_end_is_xfrm_flow(struct sk_buff *skb, struct ecm_tracker_i
 	if (ip_hdr->is_v4) {
 		if ((IPCB(skb)->flags & IPSKB_XFRM_TRANSFORMED)) {
 			DEBUG_TRACE("%px: Packet has undergone xfrm transformation\n", skb);
+			*inner = false;
 			return true;
 		}
 	} else if (IP6CB(skb)->flags & IP6SKB_XFRM_TRANSFORMED) {
 		DEBUG_TRACE("%px: Packet has undergone xfrm transformation\n", skb);
+		*inner = false;
 		return true;
 	}
 
@@ -289,6 +291,7 @@ static bool ecm_front_end_is_xfrm_flow(struct sk_buff *skb, struct ecm_tracker_i
 	 */
 	if (secpath_exists(skb)) {
 		DEBUG_TRACE("%px: Packet has undergone xfrm decapsulation((%d)\n", skb, ip_hdr->protocol);
+		*inner = true;
 		return true;
 	}
 
@@ -298,6 +301,7 @@ static bool ecm_front_end_is_xfrm_flow(struct sk_buff *skb, struct ecm_tracker_i
 	dst = skb_dst(skb);
 	if (dst && dst->xfrm) {
 		DEBUG_TRACE("%px: Plain text packet destined for xfrm(%d)\n", skb, ip_hdr->protocol);
+		*inner = true;
 		return true;
 	}
 #endif
@@ -311,7 +315,9 @@ static bool ecm_front_end_is_xfrm_flow(struct sk_buff *skb, struct ecm_tracker_i
  */
 bool ecm_front_end_feature_check(struct sk_buff *skb, struct ecm_tracker_ip_header *ip_hdr)
 {
-	if (ecm_front_end_is_xfrm_flow(skb, ip_hdr)) {
+	bool inner = 0;
+
+	if (ecm_front_end_is_xfrm_flow(skb, ip_hdr, &inner)) {
 #ifdef ECM_XFRM_ENABLE
 		struct net_device *ipsec_dev;
 		int32_t interface_type;
