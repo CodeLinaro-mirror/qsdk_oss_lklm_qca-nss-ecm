0333c65ce0851ffe4fcaab2a4dbf9b7f4c5cf278
Parikshit Gune <quic_pgune@quicinc.com>
2023-12-04 18:25:41 +0530

    [qca-nss-ecm] Enable FSE(flow search engine) provisioning for OPEN profile
    Add FSE rule add/delete for Open profile.
    
    Change-Id: Ic2880aa34d0ca6935d6e7cdce22a5b7f4a0e6d80
    Signed-off-by: Parikshit Gune <quic_pgune@quicinc.com>
    (cherry picked from commit 7a3e56584d4accdb9e4db89f5b2490aebcd62cee)

--- ecm_wifi_plugins/ecm_wifi_plugin_init.c
+++ ecm_wifi_plugins/ecm_wifi_plugin_init.c
@@ -39,13 +39,13 @@ int __init ecm_wifi_plugin_init_module(void)
 		return ret;
 	}
 
-#ifndef ECM_WIFI_PLUGIN_OPEN_PROFILE_ENABLE
 	ret = ecm_wifi_plugin_fse_cb_register();
 	if (ret) {
 		ecm_wifi_plugin_warning("FSE callback registration failed\n");
 		return ret;
 	}
 
+#ifndef ECM_WIFI_PLUGIN_OPEN_PROFILE_ENABLE
 	ret = ecm_wifi_plugin_mscs_register();
 	if (ret) {
 		ecm_wifi_plugin_warning("MSCS callback registration failed\n");
@@ -64,9 +64,9 @@ int __init ecm_wifi_plugin_init_module(void)
 static void __exit ecm_wifi_plugin_exit_module(void)
 {
 	ecm_wifi_plugin_emesh_unregister();
+	ecm_wifi_plugin_fse_cb_unregister();
 #ifndef ECM_WIFI_PLUGIN_OPEN_PROFILE_ENABLE
 	ecm_wifi_plugin_mscs_unregister();
-	ecm_wifi_plugin_fse_cb_unregister();
 #endif
 	ecm_wifi_plugin_info("ECM_WIFI_PLUGIN unloaded\n");
 }
