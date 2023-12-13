dd188cd4aed0c31be096167050d91c7117b561fd
Subhash Kumar Katnapally <quic_skatnapa@quicinc.com>
2023-12-13 18:42:23 +0530

    [qca-nss-ecm] Add source base route lookup
    Add support to pass source network for route look up.
    
    Change-Id: I4cdf4a10175cef95c842b6a875cdb19640287be5
    Signed-off-by: Subhash Kumar Katnapally <quic_skatnapa@quicinc.com>

--- frontends/cmn/ecm_ipv6.h
+++ frontends/cmn/ecm_ipv6.h
@@ -1,6 +1,8 @@
 /*
  **************************************************************************
  * Copyright (c) 2014-2016, 2020-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
@@ -32,7 +34,7 @@ extern unsigned int ecm_ipv6_ip_process(struct net_device *out_dev, struct net_d
 extern void ecm_ipv6_connection_regenerate(struct ecm_db_connection_instance *ci, ecm_tracker_sender_type_t sender,
 							struct net_device *out_dev, struct net_device *in_dev, __be16 *layer4hdr, struct sk_buff *skb);
 extern struct ecm_db_node_instance *ecm_ipv6_node_establish_and_ref(struct ecm_front_end_connection_instance *feci,
-							struct net_device *dev, ip_addr_t addr,
+							struct net_device *dev, ip_addr_t addr, ip_addr_t saddr,
 							struct ecm_db_iface_instance *interface_list[], int32_t interface_list_first,
 							uint8_t *given_node_addr, struct sk_buff *skb);
 extern struct ecm_db_host_instance *ecm_ipv6_host_establish_and_ref(ip_addr_t addr);
