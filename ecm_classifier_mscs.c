f61eff88f60cabebd504b033f8f1ef2089f812d7
Parikshit Gune <quic_pgune@quicinc.com>
2024-03-26 18:34:15 +0530

    [qca-nss-ecm] Make MSCS classifier non relevant for eth to eth flows.
    Currently MSCS classifier assumes that it will be enabled only for WLAN flows.
    In that case, MSCS classifier will not allow any eth to eth flow to be
    accelerated. Fix here is to make MSCS classifier not relevant for such flows.
    
    Change-Id: Iab82832394d6723b4539096c3af90de2dd3f8c0e
    Signed-off-by: Parikshit Gune <quic_pgune@quicinc.com>

--- ecm_classifier_mscs.c
+++ ecm_classifier_mscs.c
@@ -76,8 +76,8 @@
  * Magic numbers
  */
 #define ECM_CLASSIFIER_MSCS_INSTANCE_MAGIC 0x1234
-#define ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS 0x4
-#define ECM_CLASSIFIER_MSCS_UL_ACCEL_DELAY_PACKETS 0x14
+#define ECM_CLASSIFIER_MSCS_SCS_ACCEL_DELAY_PACKETS 0x4
+#define ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS 0x14
 #define ECM_CLASSIFIER_MSCS_INVALID_SPI 0xff
 #define ECM_CLASSIFIER_MSCS_INVALID_RULE_ID 0xffff
 
@@ -112,7 +112,8 @@ struct ecm_classifier_mscs_instance {
 #if (DEBUG_LEVEL > 0)
 	uint16_t magic;
 #endif
-	uint16_t slow_ul_pkts;                                  /* count of slow ul packets */
+	uint16_t slow_ul_pkts;                                  /* count of slow UL packets */
+	uint16_t slow_dl_pkts;                                  /* count of slow DL packets */
 };
 
 /*
@@ -480,6 +481,21 @@ static void ecm_classifier_mscs_process(struct ecm_classifier_instance *aci, ecm
 		cmscsi->process_response.relevance = ECM_CLASSIFIER_RELEVANCE_NO;
 		goto mscs_classifier_out;
 	}
+
+	/*
+	 * If this classifier has seen enough slow packets and no SCS / MSCS has to update the priority,
+	 * make the classifier not relevant.
+	 */
+	if (((cmscsi->slow_ul_pkts > ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS) ||
+			(cmscsi->slow_dl_pkts > ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS)) &&
+			!cmscsi->scs_priority_update && !cmscsi->mscs_priority_update) {
+		/*
+		 * Lock still held
+		 */
+		cmscsi->process_response.relevance = ECM_CLASSIFIER_RELEVANCE_NO;
+		goto mscs_classifier_out;
+	}
+
 	spin_unlock_bh(&ecm_classifier_mscs_lock);
 
 	/*
@@ -526,6 +542,17 @@ static void ecm_classifier_mscs_process(struct ecm_classifier_instance *aci, ecm
 	}
 
 	ecm_db_netdevs_get_and_hold(ci, sender, &src_dev, &dest_dev);
+
+	/*
+	 * Make the classifier relevance NO for non wlan flows.
+	 */
+	if (!src_dev->ieee80211_ptr && !dest_dev->ieee80211_ptr) {
+		DEBUG_TRACE("%px: Non WLAN flow, MSCS classifier is not relevant !\n", ci);
+		ecm_db_connection_deref(ci);
+		spin_lock_bh(&ecm_classifier_mscs_lock);
+		cmscsi->process_response.relevance = ECM_CLASSIFIER_RELEVANCE_NO;
+		goto mscs_classifier_out;
+	}
 
 	/*
 	 * Set the invalid SCS rule id, in case if we do not find any SCS rule.
@@ -708,6 +735,12 @@ mscs_classifier_exit:
 		spin_unlock_bh(&ecm_classifier_mscs_lock);
 	}
 
+	if (dest_dev->ieee80211_ptr) {
+		spin_lock_bh(&ecm_classifier_mscs_lock);
+		cmscsi->slow_dl_pkts++;
+		spin_unlock_bh(&ecm_classifier_mscs_lock);
+	}
+
 	if (ECM_FRONT_END_ACCELERATION_NOT_POSSIBLE(accel_mode)) {
 		DEBUG_TRACE("%x: not relevant accel_mode: %d, this is a race condition while ae switch happens from ppe to sfe\n",feci->ci->serial, accel_mode);
 	}
@@ -732,19 +765,19 @@ mscs_classifier_exit:
 	if (ecm_classifier_scs_enabled) {
 		/*
 		 * If SCS classifier is enabled, give chance to SCS first for 
-		 * ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS packets to see if it
+		 * ECM_CLASSIFIER_MSCS_SCS_ACCEL_DELAY_PACKETS packets to see if it
 		 * has to update the priority, if not fall back to MSCS.
 		 */
-		if (slow_pkts < ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS) {
+		if (slow_pkts < ECM_CLASSIFIER_MSCS_SCS_ACCEL_DELAY_PACKETS) {
 			if (!scs_rule_match && cmscsi->process_response.accel_mode != ECM_CLASSIFIER_ACCELERATION_MODE_ACCEL) {
 				cmscsi->process_response.accel_mode = ECM_CLASSIFIER_ACCELERATION_MODE_NO;
 				goto mscs_classifier_out;
 			}
 		}
 	} else {
-		if(cmscsi->slow_ul_pkts < ECM_CLASSIFIER_MSCS_UL_ACCEL_DELAY_PACKETS) {
+		if(cmscsi->slow_ul_pkts < ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS) {
 			/*
-			 * Deny acceleration for ECM_CLASSIFIER_MSCS_UL_ACCEL_DELAY_PACKETS
+			 * Deny acceleration for ECM_CLASSIFIER_MSCS_ACCEL_DELAY_PACKETS
 			 * slow uplink packets.
 			 */
 			cmscsi->process_response.accel_mode = ECM_CLASSIFIER_ACCELERATION_MODE_NO;
