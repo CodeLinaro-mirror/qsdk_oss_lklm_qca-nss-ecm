f91e8ce32ba6149a955994925dc2bbed92f6d66f
Parikshit Gune <quic_pgune@quicinc.com>
2024-06-10 17:57:20 +0530

    [qca-nss-ecm] Send Wi-Fi tag to PPE in case if Wi-Fi classifier is enabled.
    In case if Wi-Fi classifier is enabled (6.1 onwards),
    set Wi-Fi classifier flag while sending acceleration requests
    to PPE. This is to differentiate in PPE,
    from 5.4 and 6.1 kernel and act accordingly.
    
    Change-Id: I0159c5c6c623e6a587cc99ddbe6a062effd89003
    Signed-off-by: Parikshit Gune <quic_pgune@quicinc.com>

--- frontends/ppe/ecm_ppe_ported_ipv4.c
+++ frontends/ppe/ecm_ppe_ported_ipv4.c
@@ -1053,6 +1053,7 @@ static void ecm_ppe_ported_ipv4_connection_accelerate(struct ecm_front_end_conne
 	 * WIFI information
 	 * Set up the flow and return mark.
 	 */
+	pd4rc->valid_flags |= PPE_DRV_V4_VALID_FLAG_WIFI_TAG;
 	if (pr->process_actions & ECM_CLASSIFIER_PROCESS_ACTION_WIFI_TAG) {
 		if (pr->flow_mark) {
 			pd4rc->wifi_rule.flow_mark = pr->flow_mark;
