d8c7b232b05301ad8da4ca90a3d8927621830731
Pratik Tripathi <quic_pratikt@quicinc.com>
2024-12-02 18:02:07 +0530

    [qca-nss-ecm] interface status should be check before adding interface into the database
    Change-Id: Ib5238b0825f7cc95930ed0d326c436c9c32eabed
    Signed-off-by: Pratik Tripathi <quic_pratikt@quicinc.com>

--- ecm_interface.c
+++ ecm_interface.c
@@ -5423,15 +5423,19 @@ int32_t ecm_interface_heirarchy_construct(struct ecm_front_end_connection_instan
 	while (current_interface_index > 0) {
 		struct ecm_db_iface_instance *ii;
 		struct net_device *next_dev;
-		/*
-		 * Get the ecm db interface instance for the device at hand
-		 */
-		ii = ecm_interface_establish_and_ref(feci, dest_dev, skb);
 
+		/*
+		 * Check interface status before establishing interface
+		 */
 		if (!(dest_dev->flags & IFF_UP)) {
 			DEBUG_WARN("%px: dest interface(%s) is not up\n", feci, dest_dev->name);
 			goto done;
 		}
+
+		/*
+		 * Get the ecm db interface instance for the device at hand
+		 */
+		ii = ecm_interface_establish_and_ref(feci, dest_dev, skb);
 
 		/*
 		 * If the interface could not be established then we abort
