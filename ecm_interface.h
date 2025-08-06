474c6e058543752f6e6f772b4521b4cd3bb76493
Vy Vo <vvo@qti.qualcomm.com>
2025-08-05 18:31:15 -0700

    [qca-nss-ecm]: Add defunct connections by bridge port
    * Defunct connection instance based on given bridge port dev
    * Check iface in connection instance interface list and then defunct
    connection instance if matching iface is found
    
    Change-Id: Ib64999275c817fbb738296ad9b2045f95083f47a
    Signed-off-by: Vy Vo <vvo@qti.qualcomm.com>
    (cherry picked from commit 966d0a24fb463d6b3ed7ee9a8923761075ecc28c)

--- ecm_interface.h
+++ ecm_interface.h
@@ -119,6 +119,7 @@ void ecm_interface_dev_regenerate_connections(struct net_device *dev);
 struct net_device *ecm_interface_dev_find_by_local_addr(ip_addr_t addr);
 bool ecm_interface_find_gateway(ip_addr_t daddr, ip_addr_t saddr, ip_addr_t gw_addr, uint32_t skb_mark);
 void ecm_interface_dev_defunct_connections(struct net_device *dev);
+void ecm_interface_defunct_connections_by_bridge_port(struct net_device *dev);
 void ecm_interface_node_connections_defunct(uint8_t *mac, int ip_version);
 void ecm_interface_node_connections_defunct_by_type(uint8_t *mac, int ip_version, ecm_db_connection_defunct_type_t type);
 bool ecm_interface_tunnel_mtu_update(ip_addr_t saddr, ip_addr_t daddr, ecm_db_iface_type_t type, int32_t *mtu);
