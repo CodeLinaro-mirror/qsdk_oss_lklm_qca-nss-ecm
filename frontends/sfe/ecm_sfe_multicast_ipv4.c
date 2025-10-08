07836e9657e0698a72fe0a3416453a9daebe8b22
Tathagata Roy <tathroy@qti.qualcomm.com>
2025-10-08 17:45:00 +0530

    [qca-nss-ecm] Fixing compilation for ECM.
    Added missing paramater to the ecm_sfe_stats_v4_inc and ecm_sfe_stats_v6_inc api.
    
    Change-Id: I402bc8c4b374e917aaee468e1989d531d61a9108
    Signed-off-by: Tathagata Roy <tathroy@qti.qualcomm.com>

--- frontends/sfe/ecm_sfe_multicast_ipv4.c
+++ frontends/sfe/ecm_sfe_multicast_ipv4.c
@@ -746,7 +746,7 @@ static int ecm_sfe_multicast_ipv4_connection_update_accelerate(struct ecm_front_
 				to_mtu = (uint32_t)ecm_db_connection_iface_mtu_get(feci->ci, ECM_DB_OBJ_DIR_TO);
 				to_sfe_iface_id = ecm_db_iface_ae_interface_identifier_get(ii);
 				if (to_sfe_iface_id < 0) {
-					ecm_sfe_stats_v4_inc(ECM_SFE_STATS_V4_EXCEPTION_MULTICAST, ECM_SFE_STATS_V4_EXCEPTION_MULTICAST_TO_IFACE_INVALID_IFACE_ID);
+					ecm_sfe_stats_v4_inc(feci, ECM_SFE_STATS_V4_EXCEPTION_MULTICAST, ECM_SFE_STATS_V4_EXCEPTION_MULTICAST_TO_IFACE_INVALID_IFACE_ID);
 					DEBUG_TRACE("%px: to_sfe_iface_id: %d\n", feci, to_sfe_iface_id);
 					rule_invalid = true;
 					break;
@@ -1628,7 +1628,7 @@ static void ecm_sfe_multicast_ipv4_connection_accelerate(struct ecm_front_end_co
 				to_mtu = (uint32_t)ecm_db_connection_iface_mtu_get(feci->ci, ECM_DB_OBJ_DIR_TO);
 				to_sfe_iface_id = ecm_db_iface_ae_interface_identifier_get(ii);
 				if (to_sfe_iface_id < 0) {
-					ecm_sfe_stats_v4_inc(ECM_SFE_STATS_V4_EXCEPTION_MULTICAST, ECM_SFE_STATS_V4_EXCEPTION_MULTICAST_TO_IFACE_INVALID_IFACE_ID);
+					ecm_sfe_stats_v4_inc(feci, ECM_SFE_STATS_V4_EXCEPTION_MULTICAST, ECM_SFE_STATS_V4_EXCEPTION_MULTICAST_TO_IFACE_INVALID_IFACE_ID);
 					DEBUG_TRACE("%px: to_sfe_iface_id: %d\n", feci, to_sfe_iface_id);
 					rule_invalid = true;
 					break;
