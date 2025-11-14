459a2a7951509cb032eed2ad15924671cb78ba2d
Vy Vo <vvo@qti.qualcomm.com>
2025-11-13 16:14:24 -0800

    [qca-nss-ecm]: Move the conntrack confirm check after classifier checks
    This is done to handle iperf3 UDP connections. DSCP value from the first
    packet needs to be set in ECM.
    
    Change-Id: I261b3f02ed0a31014f1d6165a6956a410e2fed35
    Signed-off-by: Vy Vo <vvo@qti.qualcomm.com>

--- frontends/cmn/ecm_ported_ipv6.c
+++ frontends/cmn/ecm_ported_ipv6.c
@@ -890,17 +890,6 @@ done:
 	}
 
 	/*
-	 * Unconfirmed connection may be dropped by Linux at the final step,
-	 * So we don't allow acceleration for the unconfirmed connections.
-	 */
-	if (protocol == IPPROTO_UDP && likely(ct) && !nf_ct_is_confirmed(ct)) {
-		DEBUG_WARN("%px: Unconfirmed UDP connection\n", ct);
-		ecm_db_connection_deref(ci);
-		ecm_stats_v6_inc(ECM_STATS_V6_EXCEPTION_PORTED, ECM_STATS_V6_EXCEPTION_PORTED_UDP_CONN_NOT_CONFIRM);
-		return NF_ACCEPT;
-	}
-
-	/*
 	 * Check if AE switch is needed.
 	 */
 	if (ecm_front_end_connection_check_and_switch_to_next_ae(ci->feci)) {
@@ -1333,6 +1322,17 @@ done:
 	 */
 	skb->priority = prevalent_pr.flow_qos_tag;
 	DEBUG_TRACE("%px: skb priority: %u\n", ci, skb->priority);
+
+	/*
+	 * Unconfirmed connection may be dropped by Linux at the final step,
+	 * So we don't allow acceleration for the unconfirmed connections.
+	 */
+	if (protocol == IPPROTO_UDP && likely(ct) && !nf_ct_is_confirmed(ct)) {
+		DEBUG_WARN("%px: Unconfirmed UDP connection\n", ct);
+		ecm_db_connection_deref(ci);
+		ecm_stats_v6_inc(ECM_STATS_V6_EXCEPTION_PORTED, ECM_STATS_V6_EXCEPTION_PORTED_UDP_CONN_NOT_CONFIRM);
+		return NF_ACCEPT;
+	}
 
 	/*
 	 * Accelerate?
