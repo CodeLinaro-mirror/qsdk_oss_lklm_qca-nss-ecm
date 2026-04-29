102019b30c3e751466db06f20fdc580d7aca4661
unnatid <unnatid@qti.qualcomm.com>
2026-04-29 12:07:01 +0530

    [qca-nss-ecm] Fix to resolve rule deletion from SPM DB
    Set msg->status = true after successful AE TX in
    ECM_RULE_UPDATE_TYPE_BI_DI_SAWF_QOS handler to prevent
    EMESH classifier from deleting the SAWF rule from SPM DB.
    
    Change-Id: Iff6c9c63bb5973c9be767a3eeb67d63383629f83
    Signed-off-by: Unnati Dixit <unnatid@qti.qualcomm.com>

--- frontends/sfe/ecm_sfe_common.c
+++ frontends/sfe/ecm_sfe_common.c
@@ -921,6 +921,8 @@ void ecm_sfe_common_update_rule(struct ecm_front_end_connection_instance *feci, 
 					return;
 				}
 
+				msg->status = true;
+
 				DEBUG_TRACE("%px: sawf flow/return mark=0x%08x/0x%08x %pI4:%u -> %pI4:%u protocol=%u\n",
 						feci, update_msg->info.sawf.flow_mark, update_msg->info.sawf.return_mark,
 						update_msg->src_ip, ntohs(update_msg->src_port),
@@ -989,6 +991,8 @@ void ecm_sfe_common_update_rule(struct ecm_front_end_connection_instance *feci, 
 					kfree(msg_v6);
 					return;
 				}
+
+				msg->status = true;
 
 				DEBUG_TRACE("%px: sawf flow/return mark=0x%08x/0x%08x %pI6c@%u -> %pI6c@%u protocol=%u\n",
 						feci, update_msg->info.sawf.flow_mark, update_msg->info.sawf.return_mark,
