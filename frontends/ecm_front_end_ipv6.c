ac0cc383c12073d797d40bf4acaab1bfad673084
Pranav Vinayak Wagh <pranavvi@qti.qualcomm.com>
2026-05-22 14:30:51 -0700

    [qca-nss-ecm] Use atomic reference counting for temporary stop flag
    Replace integer with atomic_t for front-end stop_temp to mitigate race conditions during concurrent event handling in IPv4 and IPv6.
    
    Change-Id: Ifbf565b9a575314aa15c0899d765049085159446
    Signed-off-by: Pranav Vinayak Wagh <pranavvi@qti.qualcomm.com>

--- frontends/ecm_front_end_ipv6.c
+++ frontends/ecm_front_end_ipv6.c
@@ -61,7 +61,7 @@ extern int ecm_front_end_ipv6_stopped;	/* When non-zero further traffic will not
 /*
  * Temporary operational control
  */
-int ecm_front_end_ipv6_stopped_temp = 0;	/* When non-zero further traffic/process will not be processed where it is checked */
+atomic_t ecm_front_end_ipv6_stopped_temp = ATOMIC_INIT(0);	/* When non-zero further traffic/process will not be processed where it is checked */
 
 /*
  * ecm_front_end_ipv6_interface_construct_ip_addr_set()
@@ -507,7 +507,13 @@ bool ecm_front_end_ipv6_interface_construct_set_and_hold(struct sk_buff *skb, ec
  */
 void ecm_front_end_ipv6_stop_temp(int num)
 {
-	ecm_front_end_ipv6_stopped_temp = num;
+	if (num) {
+		atomic_inc(&ecm_front_end_ipv6_stopped_temp);
+	} else {
+		if (atomic_read(&ecm_front_end_ipv6_stopped_temp) > 0) {
+			atomic_dec(&ecm_front_end_ipv6_stopped_temp);
+		}
+	}
 }
 
 /*
